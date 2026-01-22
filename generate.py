30ce2939afbf059f4116cf9fa72f1ffa44612f9f
tphypers <phypersjt@gmail.com>
2026-01-22 16:04:21 -0500

    multiple block simulation created

--- generate.py
+++ generate.py
@@ -1,5 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+
+pyrosim.Start_SDF("boxes.sdf")
+for i in range(5):
+    for y in range(5):
+        dim = 1
+        for x in range(10):
+            pyrosim.Send_Cube(name=f"Box{i}{y}{x}", pos=[i+.5,y+.5,x+.5] , size=[dim,dim,dim])
+            dim = dim * .9
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,7 +1,19 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+#setting up simulation world
+#gravity
+p.setGravity(0,0,-9.8)
+#floor
+planeId = p.loadURDF("plane.urdf")
+#load world
+p.loadSDF("boxes.sdf")
+
+#running simulation world for a few seconds
 for i in range(1000):
     p.stepSimulation()
     print(i)
