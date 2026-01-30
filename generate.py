ca4208437361d84681c472888b66eb27555f6525
tphypers <phypersjt@gmail.com>
2026-01-29 19:39:13 -0500

    joints practice (hook bot)

--- generate.py
+++ generate.py
@@ -1,13 +1,29 @@
 import pyrosim.pyrosim as pyrosim
 
+#world creation
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name=f"Box", pos=[-2,-2,.5] , size=[1,1,1])
+    pyrosim.End()
 
+#Robot creation
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name=f"Link0", pos=[0,0,.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name=f"Link1", pos=[0,0,.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name=f"Link2", pos=[0,0,.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,.5,.5])
+    pyrosim.Send_Cube(name=f"Link3", pos=[0,.5,0] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0,1,0])
+    pyrosim.Send_Cube(name=f"Link4", pos=[0,.5,0] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0,.5,-.5])
+    pyrosim.Send_Cube(name=f"Link5", pos=[0,0,-.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0,0,-1])
+    pyrosim.Send_Cube(name=f"Link6", pos=[0,0,-.5] , size=[1,1,1])
 
-pyrosim.Start_SDF("boxes.sdf")
-for i in range(5):
-    for y in range(5):
-        dim = 1
-        for x in range(10):
-            pyrosim.Send_Cube(name=f"Box{i}{y}{x}", pos=[i+.5,y+.5,x+.5] , size=[dim,dim,dim])
-            dim = dim * .9
+    pyrosim.End()
 
-pyrosim.End()
+Create_Robot()
+Create_World()
--- simulate.py
+++ simulate.py
@@ -10,8 +10,10 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 #floor
 planeId = p.loadURDF("plane.urdf")
+#robot
+robotId = p.loadURDF("body.urdf")
 #load world
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 
 #running simulation world for a few seconds
 for i in range(1000):
